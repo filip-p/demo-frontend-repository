6b8589c373f0419627a81a8e084c1dd2692bb7ee
filip-p <filip_pancevski@yahoo.com>
2019-10-26 21:24:39 +0200

    added simple routing

--- src/App.js
+++ src/App.js
@@ -4,13 +4,34 @@ import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
 import 'tachyons';
 
-function App() {
-  return (
-    <div className="App">
-        <Navigation />
-        <Signin />
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'signin',
+
+    }
+  }
+
+
+  onRouteChange = (route) => {
+    this.setState({route:route});
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <Navigation onRouteChange={this.onRouteChange}/>
+          { this.state.route === 'signin'
+          ? <Signin onRouteChange={this.onRouteChange} />
+          :<div>
+
+          </div>
+          }
+        </div>  
+      )
+  } 
 }
 
 export default App;
